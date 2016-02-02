2f0ab2ca03d28f6e25b9d672ae9db53dd6362972
Dan Austria <dan.austria@appian.com>
2016-02-01 20:43:17 -0500

    CEX-3603
    Random integer between min and max
    Random decimal between min and max
    Random decimal between min and max with number of decimal points

--- src/test/java/com/appiancorp/ps/automatedtest/fixture/BaseFixtureTest.java
+++ src/test/java/com/appiancorp/ps/automatedtest/fixture/BaseFixtureTest.java
@@ -104,8 +104,25 @@ public class BaseFixtureTest {
     
     @Test
     public void testGetRandomString(){
-        //TODO Create test case
+        assertTrue(bFixture.getRandomString(7).length() == 7);
     }
+    @Test
+    public void testGetRandomIntFromTo(){
+        int randomInt = bFixture.getRandomIntegerFromTo(0, 10);
+        assertTrue((randomInt<10) && (randomInt>=0));
+    }
+    @Test
+    public void testGetRandomDecimalFromTo(){
+        double randomDec = bFixture.getRandomDecimalFromTo(0, 10);
+        assertTrue((randomDec<10) && (randomDec>=0));
+    }
+
+    @Test
+    public void testGetRandomDecimalFromToWith(){
+        double randomDec = bFixture.getRandomDecimalFromToWith(0.1, 10.01, 6);
+        assertTrue((randomDec<10.01) && (randomDec>=0.1));
+    }
+    
     
     @AfterClass
     public static void tearDown() throws Exception {
