82f02d81da0602c6380a5ef34c15ff26b011dc6c
Michael Chirlin <michael.chirlin@appian.com>
2016-07-21 00:36:01 -0400

    Resolving mac issues

--- src/main/java/com/appiancorp/ps/automatedtest/common/Constants.java
+++ src/main/java/com/appiancorp/ps/automatedtest/common/Constants.java
@@ -6,9 +6,11 @@ public class Constants {
   public final static String CHROME_BROWSER_HOME = "chrome.browser.home";
   public final static String FIREFOX_BROWSER_HOME = "firefox.browser.home";
 
+  public final static String CHROME_DRIVER_HOME = "chrome.driver.home";
   public final static String CHROME_DRIVER = "chromedriver.exe";
+  public final static String IE_DRIVER_HOME = "ie.driver.home";
   public final static String IE_DRIVER = "IEDriverServer_32.exe";
 
-  public final static String DRIVERS_LOCATION = "\\lib\\drivers\\";
+  public final static String DRIVERS_LOCATION = "/lib/drivers/";
 
 }
